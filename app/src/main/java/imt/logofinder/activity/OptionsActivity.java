4cd6b425e99c9e778cfb781dc64bbd8e02f05390
Thomas <thomas.waeters@gmail.com>
2018-02-16 15:26:06 +0100

    Modification du option layout pour ajouter une dropdownlist pour le choix du serveur. Remplissage de la DDL avec les infos en bdd et affichage du chemin du serveur choisi.

--- app/src/main/java/imt/logofinder/activity/OptionsActivity.java
+++ app/src/main/java/imt/logofinder/activity/OptionsActivity.java
@@ -5,9 +5,17 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.security.AccessController;
+import java.util.ArrayList;
+import java.util.List;
 
 import imt.logofinder.R;
 import imt.logofinder.sql.DbSchema;
@@ -18,37 +26,79 @@ import static imt.logofinder.sql.DbSchema.*;
  * Created by TOM on 15/02/2018.
  */
 
-public class OptionsActivity extends AppCompatActivity {
-    private EditText editText_nom_serveur = null;
-
+public class OptionsActivity extends AppCompatActivity implements OnItemSelectedListener {
+    private TextView textView_chemin_serveur = null;
+    private Spinner spinner_ddl_servers = null;
+    private DbHelper dbHelper = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
-        //Instanciation du TextView
+        //Instanciation des Composants
+        this.textView_chemin_serveur = (TextView) findViewById(R.id.textView_chemin_serveur);
+        this.spinner_ddl_servers = (Spinner) findViewById(R.id.spinner_ddl_servers);
 
-        this.editText_nom_serveur = (EditText) findViewById(R.id.editText_nom_serveur);
 
         //Instanciation du helper SQLite pour la DAL
-        DbHelper dbHelper = new DbHelper(getApplicationContext());
+         dbHelper = new DbHelper(getApplicationContext());
+
+
+
+
+
+        //Listeners
+        this.spinner_ddl_servers.setOnItemSelectedListener(this);
+
+
+        fillDropDownList();
+
+    }
+
+    private void fillDropDownList(){
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String[] projection = {
+                TableOption._ID,
+                TableOption.NOM_COLONNE_SERVERNAME,
+        };
+
+        Cursor cursor = db.query(TableOption.NOM_TABLE,projection,null,null,null,null,null);
+        List serverItems = new ArrayList<>();
+        while(cursor.moveToNext()){
+            String serverItem = cursor.getString(cursor.getColumnIndex(TableOption.NOM_COLONNE_SERVERNAME));
+            serverItems.add(serverItem);
+        }
+        cursor.close();
+
+        ArrayAdapter<String> adapter = new  ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,serverItems);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spinner_ddl_servers.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        String serverName = parent.getItemAtPosition(pos).toString();
+
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         String[] projection = {
                 TableOption._ID,
                 TableOption.NOM_COLONNE_SERVERNAME,
                 TableOption.NOM_COLONNE_SERVERPATH
         };
+        String selection = TableOption.NOM_COLONNE_SERVERNAME + " = ?";
+        String[] selectionArgs = { serverName };
 
-      Cursor cursor = db.query(TableOption.NOM_TABLE,projection,null,null,null,null,null);
-      cursor.moveToFirst();
-      String nom_serveur = cursor.getString(cursor.getColumnIndex(TableOption.NOM_COLONNE_SERVERNAME));
-      String path_serveur = cursor.getString(cursor.getColumnIndex(TableOption.NOM_COLONNE_SERVERPATH));
 
-        this.editText_nom_serveur.setText(nom_serveur);
+        Cursor cursor = db.query(TableOption.NOM_TABLE,projection,selection,selectionArgs,null,null,null);
+        cursor.moveToFirst();
+        String path_serveur = cursor.getString(cursor.getColumnIndex(TableOption.NOM_COLONNE_SERVERPATH));
 
+        this.textView_chemin_serveur.setText(path_serveur);
 
     }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
-
+    }
 }
